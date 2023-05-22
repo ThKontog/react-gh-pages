b9eba21d300aaf3a6eb5eb1e87ce67e34cab09f9
ThKontog <th.kontogeorg@gmail.com>
2023-05-22 23:41:24 +0300

    Updates

---
+++ precache-manifest.f21bac7c8fe593cd30cccdeed508ac0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d477e719c2d0878df66",
+    "url": "/react-gh-pages/static/css/main.8221005f.chunk.css"
+  },
+  {
+    "revision": "5d477e719c2d0878df66",
+    "url": "/react-gh-pages/static/js/main.5d477e71.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/react-gh-pages/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/react-gh-pages/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "a6558f0c85422d431291",
+    "url": "/react-gh-pages/static/js/runtime~main.a6558f0c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-pages/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2ce9c5f4471dce46f0958edf00b8f3f6",
+    "url": "/react-gh-pages/index.html"
+  }
+];
