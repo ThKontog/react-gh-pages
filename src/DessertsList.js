9f98012c22c4526b8c2f7db04f68ded909031787
ThKontog <th.kontogeorg@gmail.com>
2023-05-27 10:21:08 +0300

    first commit

---
+++ src/DessertsList.js
@@ -0,0 +1,16 @@
+function DessertsList(props) {
+    // Implement the component here.
+    const listItems = props.data
+      .filter(dessert => dessert.calories <= 500)
+      .sort((a, b) => a.calories - b.calories)
+      .map(dessert => {
+        return  <li>
+                  {dessert.calories} cal - {dessert.name}
+                </li>;
+      });
+    
+    return <ul>{listItems}</ul>;
+  }
+  
+  export default DessertsList;
+  
